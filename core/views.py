80d78cad5ee42caaf25ea9382170ddb794aa97a4
Carlos Eduardo Leal dos Santos Junior <carloseleal.prof@gmail.com>
2025-05-29 11:09:40 -0300

    Atualização de pastas e Register Login

--- core/views.py
+++ core/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, OwnerProfileForm
 from .forms import CustomUserCreationForm, OwnerProfileForm
 from properties.models import Owner
+from django.contrib import messages
 
 @login_required
 def home(request):
@@ -19,23 +20,46 @@ def register_user(request):
     else:
         form = CustomUserCreationForm()
     
-    return render(request, 'core/register_user.html', {'form': form})
+    return render(request, 'registration/register.html', {'form': form})
 
 @login_required
 def complete_owner_profile(request):
     try:
         owner = request.user.owner_profile
     except Owner.DoesNotExist:
-        owner = None
+        owner = Owner.objects.create(user=request.user)
 
     if request.method == 'POST':
         form = OwnerProfileForm(request.POST, instance=owner) 
         if form.is_valid():
-            owner_instance = form.save(commit=False)
-            owner_instance.user = request.user
-        owner_instance.save() # 
+            form.save()
+            messages.success(request,'Seu Perfil foi atualizado com sucesso!')
+
         return redirect('core:home')
     else:
         form = OwnerProfileForm(instance=owner)
 
     return render(request, 'core/complete_owner.html', {'form': form})
+
+def register_view(request):
+    if request.method == 'POST':
+        user_form = CustomUserCreationForm(request.POST)
+        owner_form = OwnerProfileForm(request.POST)
+        if user_form.is_valid() and owner_form.is_valid():
+            user = user_form.save()
+
+            owner = owner_form.save(commit=False)
+            owner.user = user
+            owner.save()
+
+            login(request,user)
+            return redirect('core/home.html')
+    else:
+        user_form = CustomUserCreationForm()
+        owner_form = OwnerProfileForm()
+
+        context = {
+            'user_form': user_form,
+            'owner_form': owner_form,
+        }
+    return render(request,'registration/register.html',context)
